4153b13cbed9bc0fb3724b26f62e797ac633c0bc
Ilya.Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-07-08 20:06:12 +0300

    Added the project skeleton.

---
+++ AuthenticationService/Controllers/AuthenticateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class AuthenticateController : ControllerBase
+    {
+        public IActionResult Authenticate(string username, string password)
+        {
+            if (_tokenManager.Authenticate())
+            {
+                // We will return a dynamic object.
+                return Ok(new
+                {
+                    Token = _tokenManager.CreateToken()
+                });
+            }
+            else
+            {
+                //ModelState.AddModelError("Unauthorized", "You are not authorized.");
+                //return new Unauthorized(ModelState);
+
+                return new UnauthorizedResult();
+            }
+        }
+    }
+}
